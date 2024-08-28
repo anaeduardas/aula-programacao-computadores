154ea53fe5971f1fcc806eddae6fcce6e43589ff
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-08-28 20:40:19 -0300

    Create banco.js

---
+++ aula 2808 condicoes/banco.js
@@ -0,0 +1,14 @@
+/*
+
+elabore um programa que:
+1. inicie o saldo da conta zerado.
+2. solicite um valor a ser depositado.
+3. solicite o valor a ser sacado.
+4. apresente se e possivel sacar ou nao o valor.
+
+*/
+
+var saldo = 0;
+var valorDepositado = parseInt(prompt("digite o valor a ser depositado"));
+var valorSacado = parseInt(prompt("digite o valor a ser sacado:"));
+if
