f3a88bdbb789e84022f980e44cfef791278c6bf9
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-09-19 22:38:09 -0300

    22.38

--- aula_02-09_condições compostas/exercicio7.js
+++ aula_02-09_condições compostas/exercicio7.js
@@ -14,4 +14,47 @@ Apresente o Menu de Bebidas:
 
 Solicite ao usuário qual Pizza e quantidade que deseja comprar, qual refrigerante e quantidade, ao final apresente o total a pagar.
 */
+var menus = parseInt(prompt(menupizzas));
+
+var menupizzas = "======= menu pizzas =====\n ";
+menupizzas = menupizzas = "numero|sabores \n ";
+menupizzas += "1 |mussarela -> R$ 20,00\n ";
+menupizzas += "2 |calabresa -> R$ 25,00\n ";
+menupizzas += "3 |margarita -> R$ 23,00\n ";
+menupizzas += "4 |4 queijos-> R$ 29,99\n ";
+menupizzas += "5 |baiana > 22,50\n ";
+
+var menubebidas = "==== menu bebeidas ====\n"
+menubebidas = menubebidas = "numero | bebidas \n ";
+menubebidas += "1 |Refrigerante Lata\n ";
+menubebidas += "2 |Refrigerante 2L\n ";
+menubebidas += "3 |Agua \n ";
+menubebidas += "4 |Suco\n ";
+
+var pizza = parseInt(prompt("Qual pizza : "));
+
+if (pizza == 1){
+    var mussarela = 20.00
+    console.log ("sabor: mussarela");
+}
+else if (pizza == 2){
+    var calabresa = 25.00
+    console.log ("sabor: calabresa");
+}
+if (pizza == 3){
+    var margarita = 23.00
+    console.log ("sabor: margarita");
+}
+if (pizza == 4){
+    var quatroqueijos = 29.99
+    console.log ("sabor: 4 queijos");
+}
+if (pizza == 5){
+    var baiana = 22.50
+    console.log ("sabor: baiana");
+}
+var bebidas = parseInt(prompt("Qual refrigerante: ")); 
+
+
+var quantidade = parseInt(prompt("qual a quantidade:  "));
 
