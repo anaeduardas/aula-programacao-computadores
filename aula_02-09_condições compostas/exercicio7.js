a940a3e2d03df8f77302315493d60a2926795a91
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-09-23 14:05:09 -0300

    Update exercicio7.js

--- aula_02-09_condições compostas/exercicio7.js
+++ aula_02-09_condições compostas/exercicio7.js
@@ -14,31 +14,69 @@ Apresente o Menu de Bebidas:
 
 Solicite ao usuário qual Pizza e quantidade que deseja comprar, qual refrigerante e quantidade, ao final apresente o total a pagar.
 */
-var menus = parseInt(prompt(menupizzas));
 
 var menupizzas = "======= menu pizzas =====\n ";
-menupizzas = menupizzas = "numero|sabores \n ";
+menupizzas = menupizzas = "cód|sabores \n ";
 menupizzas += "1 |mussarela -> R$ 20,00\n ";
 menupizzas += "2 |calabresa -> R$ 25,00\n ";
 menupizzas += "3 |margarita -> R$ 23,00\n ";
 menupizzas += "4 |4 queijos-> R$ 29,99\n ";
 menupizzas += "5 |baiana > 22,50\n ";
 
-var menubebidas = "==== menu bebeidas ====\n"
-menubebidas = menubebidas = "numero | bebidas \n ";
-menubebidas += "1 |Refrigerante Lata\n ";
-menubebidas += "2 |Refrigerante 2L\n ";
-menubebidas += "3 |Agua \n ";
-menubebidas += "4 |Suco\n ";
-
-var pizza = parseInt(prompt("Qual pizza deseja comprar : "));
+var pizza = parseInt(prompt(menupizzas));
 var quantidade = parseInt(prompt("qual a quantidade:  "));
 
-if (menupizzas == 1){
-    console.log("mussarela")
+if (pizza == 1){
+    var mussarela = 20.00
+    var  valorT1 = mussarela * quantidade;
+}
+else if (pizza == 2){
+    var calabresa = 25.00
+    var  valorT1 = calabresaa * quantidade;
+}
+else if (pizza == 3){
+    var margarita = 23.00
+    var  valorT1 = margarita * quantidade;
+}
+else if (pizza == 4 ){
+    var quatroQueijos = 29.99
+    var  valorT1 = quatroQueijos * quantidade;
+}
+else if (pizza == 5){
+    var baiana = 22.50
+    var  valorT1 = baiana * quantidade;
 }
 
 
+var menubebidas = "==== menu bebeidas ====\n"
+menubebidas = menubebidas = "cód | bebidas \n ";
+menubebidas += "1 |Refrigerante Lata -> R$ 5,00\n ";
+menubebidas += "2 |Refrigerante 2L -> R$ 10,00\n  ";
+menubebidas += "3 |Agua -> R$ 3,50\n  ";
+menubebidas += "4 |Suco -> R$ 7,50\n ";
+
+var bebida = parseInt(prompt(menubebidas));
+var quantidade2 = parseInt(prompt("qual a quantidade:  "));
+
+if (bebida == 1){
+    var refrigeranteLata = 5.00
+    var  valorT2 = refrigeranteLata * quantidade;
+}
+else if (bebida == 2){
+    var refrigerante2l = 10.00
+    var  valorT2 = refrigerante2l * quantidade;
+}
+else if (bebida == 3){
+    var agua = 3.50
+    var  valorT2 = agua * quantidade;
+}
+else if (bebida == 4){
+    var suco = 7.50
+    var  valorT2 = suco * quantidade;
+}
+
+var valorFinal = valorT1 + valorT2;
+console.log ("O total foi de: " + valorFinal );
 
 
 
