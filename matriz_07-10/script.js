5cf52a5a2569b3522364c3e0c11aa560fd04724b
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-10-07 21:40:52 -0300

    07

---
+++ matriz_07-10/script.js
@@ -0,0 +1,9 @@
+// [] criam um vetor/array/matriz
+var vetor = [];
+
+// para adicionar um elemento colocamos sua posição.
+vetor[0] = " primeira coisa";
+vator [1] = 51;
+
+// para exibir o conteúdo de um array, devemos chamar sua posição 
+console.log("posição 0 ", vetor [0]); //imprime "primeira coisa"
