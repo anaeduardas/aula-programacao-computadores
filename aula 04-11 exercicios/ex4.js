51d41412e57ccb618362d8311d0d2b00fb717d02
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-11-04 22:27:35 -0300

    fd

--- aula 04-11 exercicios/ex4.js
+++ aula 04-11 exercicios/ex4.js
@@ -3,5 +3,23 @@ elabore um programa que receba uma lista de receitas, depois receba uma lista de
 ao final, some todas as receitas e despesas, exiba o total e apresente a diferença entre eles.
 */ 
 
-var receitas = 
+var receitas = []
 
+var i = 0
+
+if (i == receitas); 
+while (i)
+
+
+
+
+var i = 0
+if (i == receitas);
+while  (i)
+
+
+
+
+var Listadespesas = prompt("entregue a lista com todaas as despesas:");
+
+console.log
