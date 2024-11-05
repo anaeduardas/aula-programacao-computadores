d6d394e8c97e7b4693d4888b3630d67bfee4a779
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-11-04 21:58:00 -0300

    estudar!!

---
+++ aula 04-11 exercicios/ex4.js
@@ -0,0 +1,7 @@
+/*
+elabore um programa que receba uma lista de receitas, depois receba uma lista de despesas.
+ao final, some todas as receitas e despesas, exiba o total e apresente a diferença entre eles.
+*/ 
+
+var receitas = 
+
