0c6e7911383b21fb1f943d404144970131b43bb7
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-11-18 22:23:18 -0300

    rr

--- aula 18-11/ex2.js
+++ aula 18-11/ex2.js
@@ -8,14 +8,12 @@ var bebidas = [
 
 { codigo: 1, nome: "guaraná", valor : 5},
 { codigo: 2, nome: "Água", valor: 6},
-{ codigp: 3, nome: "Suco", valor: 4},
+{ codigo: 3, nome: "Suco", valor: 4},
 { codigo: 4, nome: "Cerveja", valor: 7}
 
 ];
 
-var i = 0
-
-while ( i < bebidas.lenght ){
-    console.log (bebidas[i].codigo ); 
-    
+console.log ("Codigo | nome | valor ");
+for (var i = 0; i < bebidas.length; i++){
+    console.log (bebidas [i].codigo, " | ", bebidas[i].nome, " | ", bebidas[i].valor);
 }
