393aae66766ce55c49fb59bd81975c98f50d8f71
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-11-18 22:13:31 -0300

    fff

---
+++ aula 18-11/ex1.js
@@ -0,0 +1,32 @@
+/*
+Elabore um programa que contenha uma losta de produtos:
+1 - Hamburguer - R$ 10
+2 - X- salada - R$ 15
+3 - X Bacon - R$ 20
+
+solicite um código de produtos, percorra a lista e exiba seus dados
+*/
+
+/* 
+elabore um programa de cadastro de bebidas com código, nome e valor;
+ao final, percorra a lista e exiba os dados.
+
+*/
+
+/* 
+
+ Elabore um programa que apresente e solicite qual lanche o usuário deseja adicionar, pergunte a quantidade e adicione a uma lista de pedido.
+Pergunte se deseja adicionar mais lanches e refaça o processo se positivo.
+
+Apresente a lista e solicite se o usuário deseja bebida, solicite a quantidade e adicione a mesma lista de pedidos, solicite se deseja mais produtos.
+
+Ao final, apresente o pedido com todos os itens, total dos lanches, total das bebidas e total do pedido.
+
+Bonus: adicione formas de pagamento, ex: A Vista com 5% de desconto e valor para frete caso seja entrega.
+
+
+ */
+
+var listalanches = [
+    {codigo: 1, nome: ""}
+]
---
+++ aula 18-11/ex2.js
@@ -0,0 +1,21 @@
+/* 
+elabore um programa de cadastro de bebidas com código, nome e valor;
+ao final, percorra a lista e exiba os dados.
+
+*/
+
+var bebidas = [
+
+{ codigo: 1, nome: "guaraná", valor : 5},
+{ codigo: 2, nome: "Água", valor: 6},
+{ codigp: 3, nome: "Suco", valor: 4},
+{ codigo: 4, nome: "Cerveja", valor: 7}
+
+];
+
+var i = 0
+
+while ( i < bebidas.lenght ){
+    console.log (bebidas[i].codigo ); 
+    
+}
